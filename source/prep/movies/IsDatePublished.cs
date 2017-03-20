7ee36b1f8b2138505af793175cbb25f0ac988609
Lucas Cramer <LucasC@iqmetrix.com>
2017-03-20 12:37:03 -0700

    Pushing new changes

---
+++ source/prep/movies/IsDatePublished.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using code.utility.matching;
+
+namespace code.prep.movies
+{
+    public class IsDatePublished : IMatchA<Movie>
+    {
+        private readonly int year;
+        private readonly DateComparer dateComparer;
+
+        public IsDatePublished(int year, DateComparer comparer)
+        {
+            this.year = year;
+            this.dateComparer = comparer;
+        }
+
+        public bool matches(Movie item)
+        {
+            var movieYear = item.date_published.Year;
+
+            switch (dateComparer)
+            {
+                case DateComparer.After:
+                    return movieYear > year;
+                case DateComparer.Before:
+                    return movieYear < year;
+                case DateComparer.OnOrAfter:
+                    return movieYear >= year;
+                case DateComparer.OnOrBefore:
+                    return movieYear <= year;
+            }
+
+            return item.date_published.Year < year;
+        }
+    }
+
+    public enum DateComparer
+    {
+        After,
+        Before,
+        OnOrAfter,
+        OnOrBefore
+    }
+}
